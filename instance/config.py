a834366bdfdfcb206252567063de705903bfd136
Masaki Kagesawa <mk5986@nyu.edu>
2018-03-02 17:50:13 +0800

    add config for travis CI

---
+++ instance/config.py
@@ -0,0 +1,58 @@
+import pymysql
+import os
+
+
+class Config:
+    SECRET_KEY = os.environ.get('SECRET_KEY') or 'hard to guess string'
+    #SQLALCHEMY_COMMIT_ON_TEARDOWN = True
+    #FLASKY_MAIL_SUBJECT_PREFIX = '[Flasky]'
+    #FLASKY_MAIL_SENDER = 'Flasky Admin <flasky>@example.com'
+    #FLASKY_ADMIN = os.environ.get('FLASKY_ADMIN')
+
+    @staticmethod
+    def init_app(app):
+        pass
+
+
+class ProductionConfig(Config):
+    DEBUG = False
+    TESTING = False
+    DATABASE_URI = 'sqlite://:memory:'
+
+
+class TestingConfig(Config):
+    TESTING = True
+
+
+class DevelopmentConfig(Config):
+    DEBUG = True
+    SECRET_KEY = ''
+    MAIL_SERVER = 'smtp.yeah.net'
+    MAIL_PORT = 465
+    MAIL_USE_TLS = False
+    MAIL_USERNAME = 'nyusher@yeah.net'
+    MAIL_PASSWORD = os.environ['mailpwd']
+    MAIL_DEBUG = False
+    MAIL_USE_SSL = True
+
+
+config = {
+    'development': DevelopmentConfig,
+    'testing': TestingConfig,
+    'production': ProductionConfig,
+
+    'default': DevelopmentConfig
+}
+
+VERBOSE = True
+
+DB = {'host': 'nyusher.nya.vc', 'port': os.environ['DBport'],
+                         'user':'root',
+                         'password': os.environ['DBpwd'],
+                         'db':'NYUSHer',
+                         'charset': 'utf8',
+                         'cursorclass': pymysql.cursors.DictCursor}
+
+DOMAIN = 'nyusher.nya.vc'
+PORT = 6680
+PROTOCOL = "https://"
